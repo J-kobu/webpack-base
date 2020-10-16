65f5632ed4660dda1cc10abd7058016462636591
Jacob <aguilar.campos.jacob@gmail.com>
2020-10-16 15:32:22 -0300

    Primer commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import { saludar } from './js/componentes.js'; //De esta forma hacemos referencia a otro archivo y su función
+import './styles.css';
+
+
+
+const nombre = 'Jacob'
+
+saludar(nombre);
